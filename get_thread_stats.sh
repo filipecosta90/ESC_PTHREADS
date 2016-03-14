34b1ff09e5fc0385a91d8f950455b61c4e0a57fa
filipecosta90 <filipecosta.90@gmail.com>
2016-03-14 17:18:20 +0000

    preparing to run on search

--- get_thread_stats.sh
+++ get_thread_stats.sh
@@ -6,14 +6,10 @@ cd __csv
 for file in DTRACE*.csv
 do
   echo $file
-  gawk -f ../thread_creation.gawk $file > ../"THREAD_"$file
+  gawk -f ../thread_creation.gawk $file > ../"UNSORTED_"$file
+  sort ../"UNSORTED_"$file > ../"THREAD_"$file
+  rm ../"UNSORTED_"$file
 done
 cd ..
 
 mv THREAD_* __thread_dtrace
-
-echo "sorting"
-for file in __thread_dtrace/*
-do 
-  sort $file > "sorted_"$file
-done
