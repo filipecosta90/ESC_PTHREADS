4d2c674470713dbd274416e154619f0b918b0be7
filipecosta90 <filipecosta.90@gmail.com>
2016-03-14 00:39:38 +0000

    mutex algorithm done

--- trapezoid.c
+++ trapezoid.c
@@ -18,15 +18,18 @@
 #include "timer.h"
 
 /* global variables: accessible to all threads */
-int     thread_count;
-double  interval_a, interval_b;
+long   thread_count;
+long  interval_a, interval_b;
 double approx;
-int     n_intervals;
+long   n_intervals;
 double global_time_start, global_time_stop, total_time;
 char method[10];
 
 /* busy_wait global variables: */
 int flag;
+
+/* mutex global variables:*/
+pthread_mutex_t   mutex;
 
 /*  */
 double f(double x) {
@@ -35,7 +38,7 @@ double f(double x) {
   return return_val; 
 }
 
-/*  */
+/* busy wait algorithm */
 void* busy_wait_compute (  void* rank ) {
   long my_rank = (long) rank;
   double h = (interval_b - interval_a)/ n_intervals;
@@ -43,15 +46,35 @@ void* busy_wait_compute (  void* rank ) {
   long my_init_pos  = my_n * my_rank;
   long my_final_pos = my_init_pos + my_n;
   double x_i;
+
   for (long  i = my_init_pos ;i<=my_final_pos-1;i++) { 
-    while ( flag != my_rank );
     x_i = interval_a + i*h;
+    while ( flag != my_rank );
     approx  +=  f(x_i); 
     flag = (flag+1) % thread_count;
   }
+
   return NULL;
 } 
 
+/* mutex algorithm */
+void* mutex_compute (  void* rank ) {
+  long my_rank = (long) rank;
+  double h = (interval_b - interval_a)/ n_intervals;
+  long my_n = n_intervals / thread_count;
+  long my_init_pos  = my_n * my_rank;
+  long my_final_pos = my_init_pos + my_n;
+  double x_i;
+
+  for (long  i = my_init_pos ;i<=my_final_pos-1;i++){ 
+    x_i = interval_a + i*h;
+    pthread_mutex_lock(&mutex);
+    approx  +=  f(x_i); 
+    pthread_mutex_unlock(&mutex);
+  }
+
+  return NULL;
+} 
 
 void print_results( int thread_count , char* method ){
   FILE * fp;
@@ -65,6 +88,8 @@ int main(int argc, char* argv[]) {
   pthread_t*  thread_handles;
   /* the approximate value */
   approx = 0.0;
+  /* the first thread to have access to the critical section in busy wait is thread #0*/
+  flag = 0;
   if (argc != 6) {
     fprintf(stderr, "invalid arg count\nusage: ./pth_trap <number of threads> <method> <a> <b> <n>\n");
     exit(0);
@@ -82,13 +107,15 @@ int main(int argc, char* argv[]) {
 
   GET_TIME(global_time_start);
   if ( strcmp("mutex",method) == 0 ) {
-
-    printf("mutex");
-
+    /* Initialize the mutex */
+    pthread_mutex_init(&mutex, NULL);
+    for (long thread_number = 0; thread_number < thread_count; thread_number++) {
+      pthread_create(&thread_handles[thread_number], NULL, mutex_compute,(void*) thread_number);
+    }
+    pthread_mutex_destroy(&mutex);
   }
   if ( strcmp("semaphore",method) == 0 ) {
 
-    printf("semaphore");
   }
 
   if ( strcmp("busy_wait",method) == 0 ) {
@@ -96,11 +123,12 @@ int main(int argc, char* argv[]) {
       pthread_create(&thread_handles[thread_number], NULL, busy_wait_compute,(void*) thread_number);
     }
 
-    /* Wait for threads to complete. */
-    for (int i = 0; i < thread_count; i++) {
-      pthread_join(thread_handles[i], NULL);
-    }
   }
+  /* Wait for threads to complete. */
+  for (int i = 0; i < thread_count; i++) {
+    pthread_join(thread_handles[i], NULL);
+  }
+
   free(thread_handles);
   GET_TIME(global_time_stop);
   print_results( thread_count , method );
