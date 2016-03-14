f9bda5e7949fdd5ba5a64785108a0356ae2ff74f
filipecosta90 <filipecosta.90@gmail.com>
2016-03-14 12:50:44 +0000

    added dtrace commands. need to fix a bug in run_kernels and its ready to testgit add --all

--- lwptime.d
+++ lwptime.d
@@ -1,14 +1,11 @@
 dtrace:::BEGIN { 
-start = timestamp; 
-printf("Hello ESC ! %d\n", start);
-
+  start = timestamp; 
+  printf("MAIN THREAD START:\nSTART CSV:,%d\n", start);
 }
 
 proc:::lwp-create
 {
-/* printf("%s, %d \t,started at: %d, %d after first \n", stringof(args[1]->pr_fname), args[0]->pr_lwpid, timestamp, timestamp-start );
-*/
-   }
+}
 
 proc:::lwp-start
 {
@@ -17,11 +14,18 @@ proc:::lwp-start
 }
 
 proc:::lwp-exit
+/tid != 1/
+
 {
   self->end = timestamp;
   self->duration = self->end -  self->start;
-  printf ( "tid %d, started %d, ended %d , duration %d,  difference from start %d\n", tid, self->start, self->end , self->duration, self->difference_start );
-/* @[tid, stringof(curpsinfo->pr_fname)] =sum(timestamp - self->start);
-  @[tid, stringof(curpsinfo->pr_fname)] =sum(self->difference_start);
- */ self->start = 0;
+  printf ( "THREAD EXIT:\nTHREAD CSV:,%d, %d,%d ,%d,%d\n", tid, self->start, self->end , self->duration, self->difference_start );
+  self->start = 0;
 }
+
+dtrace:::END { 
+  end = timestamp; 
+  printf("MAIN THREAD END:\nEXIT CSV:,%d\n", end);
+}
+
+
--- run_kernels.sh
+++ run_kernels.sh
@@ -1,2 +1,16 @@
 #!bin/bash
-dtrace -s lwptime.d -c './pth_trap 10 semaphore 1 0 1000'
+
+for method in semaphore mutex busy_wait
+do
+  for nthreads in 1 2 4 8 16 32 64 128
+  do
+    for size in 1024 2048 4096 8192
+    do
+      var int_a = 0;
+      var int_b = $size;
+      var int_n = $nthreads;
+      dtrace -s lwptime.d -c "./pth_trap $nthreads $method $int_a $int_b $int_n" >> "DTRACE_"$method"_"$nthreads"_"$int_a"_"$int_b"_"$int_n".csv"
+    done
+  done
+done
+
